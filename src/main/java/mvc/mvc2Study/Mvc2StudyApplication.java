04636bc0034b5770171dc9b37c9d93f60e634537
최영찬 <gks54410@gmail.com>
2022-03-31 23:25:50 +0900

    Initial Commit

---
+++ src/main/java/mvc/mvc2Study/Mvc2StudyApplication.java
@@ -0,0 +1,13 @@
+package mvc.mvc2Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc2StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc2StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mvc/mvc2Study/Mvc2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.mvc2Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc2StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
