4ed2a864a5a9ca67a912c9c32c01dec28b902e8b
최영찬 <gks54410@gmail.com>
2022-04-01 18:30:19 +0900

    Feat: View Template 기능 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,26 @@
+package mvc.mvc2Study.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1(){
+        ModelAndView modelAndView = new ModelAndView("response/hello")
+                .addObject("data", "hello");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model){
+        model.addAttribute("data", "hello");
+
+        return "response/hello";
+    }
+}
