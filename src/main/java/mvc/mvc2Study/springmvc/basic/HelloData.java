a361c51e9cfc49c9ad19e373fa00dfc203a20781
최영찬 <gks54410@gmail.com>
2022-04-01 18:12:11 +0900

    Fix: ObjectMapper의 readValue()를 위해 인자가 없는 생성자 추가

--- src/main/java/mvc/mvc2Study/springmvc/basic/HelloData.java
+++ src/main/java/mvc/mvc2Study/springmvc/basic/HelloData.java
@@ -2,9 +2,11 @@ package mvc.mvc2Study.springmvc.basic;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class HelloData {
 
     private String username;
