eeba64afd1fd3dcaf25d3bcbc1ff48c565679c86
최영찬 <gks54410@gmail.com>
2022-04-01 21:00:46 +0900

    Feat: 상품 도메인 생성

---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/domain/Item.java
@@ -0,0 +1,22 @@
+package mvc.mvc2Study.springmvc.itemService.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
