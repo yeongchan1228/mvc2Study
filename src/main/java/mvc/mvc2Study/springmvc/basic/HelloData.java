2e4e85e5e37226f883a2827b44254cee75b6c4b4
최영찬 <gks54410@gmail.com>
2022-04-01 17:35:45 +0900

    Feat: HelloData 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/basic/HelloData.java
@@ -0,0 +1,12 @@
+package mvc.mvc2Study.springmvc.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloData {
+
+    private String username;
+    private int age;
+}
