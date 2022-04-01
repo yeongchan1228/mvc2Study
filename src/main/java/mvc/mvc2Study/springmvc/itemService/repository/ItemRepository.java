12a9d6a32cb4fceefec6b1dc787ef0e0cae7bf49
최영찬 <gks54410@gmail.com>
2022-04-01 21:01:13 +0900

    Feat: 상품 등록, 조회, 업데이트, 삭제 기능 조회

---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/repository/ItemRepository.java
@@ -0,0 +1,43 @@
+package mvc.mvc2Study.springmvc.itemService.repository;
+
+import mvc.mvc2Study.springmvc.itemService.domain.Item;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        Item findItem = store.get(id);
+        return findItem;
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+
+    public void update(Long id, Item upgradeParam){
+        Item findItem = store.get(id);
+        findItem.setItemName(upgradeParam.getItemName());
+        findItem.setPrice(upgradeParam.getPrice());
+        findItem.setQuantity(upgradeParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
