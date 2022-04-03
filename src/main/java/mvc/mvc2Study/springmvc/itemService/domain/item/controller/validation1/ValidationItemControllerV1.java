40fcb899eff600a510c78fcb54511f12833969da
최영찬 <gks54410@gmail.com>
2022-04-03 15:21:01 +0900

    Refactor: basic -> validation1 변경

---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/controller/validation1/ValidationItemControllerV1.java
@@ -0,0 +1,98 @@
+package mvc.mvc2Study.springmvc.itemService.domain.item.controller.validation1;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mvc.mvc2Study.springmvc.itemService.domain.item.entity.Item;
+import mvc.mvc2Study.springmvc.itemService.domain.item.repository.ItemRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/validation1/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV1 {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "validation1/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation1/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "validation1/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 오류 결과 보관
+        Map<String, String> errors = new HashMap<>();
+
+        //검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            errors.put("price", "가격은 1000원에서 1000000원까지 허용합니다");
+        }
+        if(item.getQuantity() > 9999 || item.getQuantity() == null) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        // 복합 룰 검증
+       if(item.getQuantity() != null && item.getPrice() != null){
+           int result = item.getQuantity() * item.getPrice();
+           if(result < 10000){
+               errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이여야 합니다. 현재 값 = " + result);
+           }
+       }
+
+       // 검증 실패 시 다시 입력 폼으로
+        if(!errors.isEmpty()){
+            log.info("errors = {}", errors);
+            model.addAttribute("errors", errors);
+            return "validation1/addForm";
+        }
+
+        // 성공 시
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation1/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "validation1/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/validation1/items/{itemId}";
+    }
+
+
+}
