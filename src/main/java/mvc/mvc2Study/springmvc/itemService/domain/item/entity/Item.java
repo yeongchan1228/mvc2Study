971ac0de9413a9ac98764656753d479ca03ae45c
최영찬 <gks54410@gmail.com>
2022-04-04 23:32:42 +0900

    Feat: Bean Validation 삭제

--- src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/entity/Item.java
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/entity/Item.java
@@ -17,18 +17,18 @@ import javax.validation.constraints.NotNull;
 //        message = "총합이 10000 넘게 입력해 주세요.") -> 사용하지 않는다. 기능이 너무 약함. 이 부분은 자바 코드로 작성.
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백X") // 빈칸 + 공백
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백X") // 빈칸 + 공백
     private String itemName;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000) // hibernate Validator가 동작할 때만 적용됨
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000) // hibernate Validator가 동작할 때만 적용됨
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = SaveCheck.class)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item(String itemName, Integer price, Integer quantity) {
