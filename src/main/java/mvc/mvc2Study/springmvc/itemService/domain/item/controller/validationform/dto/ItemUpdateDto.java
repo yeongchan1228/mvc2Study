5071d4c901b571e1c464cd1b138746cd40f1f55a
최영찬 <gks54410@gmail.com>
2022-04-04 23:33:26 +0900

    Feat: Item 수정을 위한 Form 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/controller/validationform/dto/ItemUpdateDto.java
@@ -0,0 +1,24 @@
+package mvc.mvc2Study.springmvc.itemService.domain.item.controller.validationform.dto;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ItemUpdateDto {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private int price;
+
+    private Integer quantity;
+}
