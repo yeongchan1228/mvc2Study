aa749f0139fe5191ca2b09ea9312b24355dc61c0
최영찬 <gks54410@gmail.com>
2022-04-04 23:07:24 +0900

    Feat: 분리를 위한 인터페이스 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/entity/SaveCheck.java
@@ -0,0 +1,4 @@
+package mvc.mvc2Study.springmvc.itemService.domain.item.entity;
+
+public interface SaveCheck{ // 저장용 그룹
+}
---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/entity/UpdateCheck.java
@@ -0,0 +1,4 @@
+package mvc.mvc2Study.springmvc.itemService.domain.item.entity;
+
+public interface UpdateCheck { // 수정용 체크
+}
