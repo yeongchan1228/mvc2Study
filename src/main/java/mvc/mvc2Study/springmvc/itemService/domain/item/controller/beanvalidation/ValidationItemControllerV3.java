e5d5eeeaa2b87f43c4afde42dc049ea4557ae77b
최영찬 <gks54410@gmail.com>
2022-04-04 23:08:44 +0900

    Feat: Validated groups 적용

--- src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/controller/beanvalidation/ValidationItemControllerV3.java
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/domain/item/controller/beanvalidation/ValidationItemControllerV3.java
@@ -3,6 +3,8 @@ package mvc.mvc2Study.springmvc.itemService.domain.item.controller.beanvalidatio
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mvc.mvc2Study.springmvc.itemService.domain.item.entity.Item;
+import mvc.mvc2Study.springmvc.itemService.domain.item.entity.SaveCheck;
+import mvc.mvc2Study.springmvc.itemService.domain.item.entity.UpdateCheck;
 import mvc.mvc2Study.springmvc.itemService.domain.item.repository.ItemRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -41,8 +43,33 @@ public class ValidationItemControllerV3 {
         return "validation3/addForm";
     }
 
+//    @PostMapping("/add")
+    public String addItemV1(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes, Model model) {
+
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int sum = item.getPrice() * item.getQuantity();
+            if(sum <= 10000){
+                bindingResult.reject("totalPriceMin", new Object[]{10000, sum}, null);
+            }
+        }
+
+        // 검증 실패 시 다시 입력 폼으로
+        if(bindingResult.hasErrors()){
+            log.info("bindingResult = {}", bindingResult);
+            // bindingResult는 알아서 Model에 담긴다.
+            return "validation3/addForm";
+        }
+
+        // 성공 시
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation3/items/{itemId}";
+    }
+
     @PostMapping("/add")
-    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult,
+    public String addItemV2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes, Model model) {
 
         if(item.getPrice() != null && item.getQuantity() != null){
@@ -74,7 +101,7 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+    public String edit(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
         if(item.getPrice() != null && item.getQuantity() != null){
             int sum = item.getPrice() * item.getQuantity();
             if(sum <= 10000){
