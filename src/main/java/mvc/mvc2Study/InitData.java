a1a04cbd772a137801897167e97f75e2f81e52ab
최영찬 <gks54410@gmail.com>
2022-04-05 14:58:14 +0900

    Feat: Member 데이터 추가

--- src/main/java/mvc/mvc2Study/InitData.java
+++ src/main/java/mvc/mvc2Study/InitData.java
@@ -3,19 +3,27 @@ package mvc.mvc2Study;
 import lombok.RequiredArgsConstructor;
 import mvc.mvc2Study.springmvc.itemService.domain.item.entity.Item;
 import mvc.mvc2Study.springmvc.itemService.domain.item.repository.ItemRepository;
+import mvc.mvc2Study.springmvc.itemService.domain.member.entity.Member;
+import mvc.mvc2Study.springmvc.itemService.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
 @RequiredArgsConstructor
-public class InitItem {
+public class InitData {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     @PostConstruct
-    public void init(){
+    public void initItem(){
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+    @PostConstruct
+    public void initMember(){
+        memberRepository.save(new Member("test", "테스터", "test"));
     }
 }
