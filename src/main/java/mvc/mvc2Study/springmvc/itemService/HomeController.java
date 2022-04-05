4282e9df157a6ffe664881cac745166b35bb5cbb
최영찬 <gks54410@gmail.com>
2022-04-05 15:48:28 +0900

    Feat: 로그인 회원, 비로그인 회원 분리 기능 추가

--- src/main/java/mvc/mvc2Study/springmvc/itemService/HomeController.java
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/HomeController.java
@@ -1,15 +1,40 @@
 package mvc.mvc2Study.springmvc.itemService;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import mvc.mvc2Study.springmvc.itemService.domain.member.entity.Member;
+import mvc.mvc2Study.springmvc.itemService.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model){
+        if(memberId == null){
+            return "home";
+        }
+
+        // 로그인된 사용자
+        Member findMember = memberRepository.findById(memberId);
+        if(findMember == null){
+            return "home";
+        }
+        // 성공 로직
+        model.addAttribute("member", findMember);
+        return "loginHome";
+
     }
 }
