c014c18a458e39ded8d8ea028ae278745f3971b8
최영찬 <gks54410@gmail.com>
2022-04-06 20:46:55 +0900

    Feat: LogInterceptor 등록 추가

--- src/main/java/mvc/mvc2Study/springmvc/exception/servlet/config/WebConfig.java
+++ src/main/java/mvc/mvc2Study/springmvc/exception/servlet/config/WebConfig.java
@@ -1,9 +1,11 @@
 package mvc.mvc2Study.springmvc.exception.servlet.config;
 
 import mvc.mvc2Study.springmvc.exception.servlet.filter.LogFilter;
+import mvc.mvc2Study.springmvc.exception.servlet.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +14,15 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .addPathPatterns("/**")
+                .order(1)
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); // 오류 페이지 경로 추가
+    }
+
+    //    @Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
