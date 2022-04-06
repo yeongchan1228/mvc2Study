6944293952a0fac0e39b8c94fd43ee96d19da1c1
최영찬 <gks54410@gmail.com>
2022-04-06 14:51:46 +0900

    Feat: Session 정보 확인 기능 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/web/session/SessionInfoController.java
@@ -0,0 +1,34 @@
+package mvc.mvc2Study.springmvc.itemService.web.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
+@Slf4j
+@RestController
+public class SessionInfoController {
+
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+
+        if(session == null)
+            return "세션이 없습니다.";
+
+        session.getAttributeNames()
+                .asIterator()
+                .forEachRemaining(name -> log.info("session name = {}, value = {}", name, session.getAttribute(name)));
+
+        log.info("sessionId = {}", session.getId());
+        log.info("session.getMaxInactiveInterval() = {}", session.getMaxInactiveInterval());
+        log.info("session.getCreateTime = {}", new Date(session.getCreationTime()));
+        log.info("session.getLastAccessedTime = {}", new Date(session.getLastAccessedTime()));
+        log.info("session.isNew() = {}", session.isNew());
+
+        return "세션 출력";
+    }
+}
