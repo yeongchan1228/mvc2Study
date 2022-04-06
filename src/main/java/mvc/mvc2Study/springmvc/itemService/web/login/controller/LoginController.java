6f2dc5fdcffc7c34f75d0a7541bbbe42a46b5401
최영찬 <gks54410@gmail.com>
2022-04-06 12:10:23 +0900

    Feat: HttpServlet Session을 사용한 로그인 처리 기능 추가

--- src/main/java/mvc/mvc2Study/springmvc/itemService/web/login/controller/LoginController.java
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/web/login/controller/LoginController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mvc.mvc2Study.springmvc.itemService.domain.login.service.LoginService;
 import mvc.mvc2Study.springmvc.itemService.domain.member.entity.Member;
+import mvc.mvc2Study.springmvc.itemService.web.SessionConst;
 import mvc.mvc2Study.springmvc.itemService.web.login.entity.dto.LoginDto;
 import mvc.mvc2Study.springmvc.itemService.web.session.SessionManager;
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -58,7 +60,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV2(@Validated @ModelAttribute("loginDto") LoginDto loginDto,
                           BindingResult bindingResult, HttpServletResponse response){
         if(bindingResult.hasErrors())
@@ -78,9 +80,44 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+    @PostMapping("/login")
+    public String loginV3(@Validated @ModelAttribute("loginDto") LoginDto loginDto,
+                          BindingResult bindingResult, HttpServletRequest request){
+        if(bindingResult.hasErrors())
+            return "login/loginForm";
+
+        Member loginMember = loginService.login(loginDto.getLoginId(), loginDto.getPassword());
+        if(loginMember == null){
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호 오류 입니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리 TODO
+        // 세션이 있으면 있는 세션 반환, 세션이 없으면 생성해서 반환
+        // creat 옵션 default -> true
+        // creat false -> 세션이 없을 시 생성 X
+        HttpSession session = request.getSession();
+        // 세션에 로그인 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+
+        return "redirect:/";
+    }
+
+//    @PostMapping("/logout")
     public String logoutV2(HttpServletRequest request){
         sessionManager.expire(request);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request){
+        // true면 생성 시 세션을 만들기 때문에 false
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
 
         return "redirect:/";
     }
@@ -90,4 +127,5 @@ public class LoginController {
         cookie.setMaxAge(0);
         response.addCookie(cookie);
     }
+
 }
