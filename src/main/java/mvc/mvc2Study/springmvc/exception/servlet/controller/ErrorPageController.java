8f5274d37968083fc00df8638646de55b200ebaa
최영찬 <gks54410@gmail.com>
2022-04-06 20:37:42 +0900

    Feat: ErrorPage 호출 처리를 위한 Controller

---
+++ src/main/java/mvc/mvc2Study/springmvc/exception/servlet/controller/ErrorPageController.java
@@ -0,0 +1,45 @@
+package mvc.mvc2Study.springmvc.exception.servlet.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+public class ErrorPageController {
+
+    @RequestMapping("/error-page/404")
+    public String errorPage404(HttpServletRequest request, HttpServletResponse response){
+        log.info("error404");
+        printErrorInfo(request);
+        return "error-page/404";
+    }
+
+    @RequestMapping("/error-page/500")
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response){
+        log.info("error500");
+        printErrorInfo(request);
+        return "error-page/500";
+    }
+
+    @RequestMapping("/error-page/ex")
+    public String errorPageEx(HttpServletRequest request, HttpServletResponse response){
+        log.info("errorEx");
+        printErrorInfo(request);
+        return "error-page/ex";
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: ex=", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(RequestDispatcher.ERROR_MESSAGE)); // ex의 경우 NestedServletException 스프링이 한번 감싸서 반환
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(RequestDispatcher.ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+        log.info("dispatchType={}", request.getDispatcherType());
+    }
+}
