5745932484ec8d92aaa105554cb039f46329237a
최영찬 <gks54410@gmail.com>
2022-04-06 18:49:48 +0900

    Feat: LoginMemberArgumentResolver 설정 추가

--- src/main/java/mvc/mvc2Study/springmvc/itemService/web/config/WebConfig.java
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/web/config/WebConfig.java
@@ -1,19 +1,26 @@
 package mvc.mvc2Study.springmvc.itemService.web.config;
 
+import mvc.mvc2Study.springmvc.itemService.web.common.LoginMemberArgumentResolver;
 import mvc.mvc2Study.springmvc.itemService.web.filter.LogFilter;
 import mvc.mvc2Study.springmvc.itemService.web.filter.LoginCheckFilter;
 import mvc.mvc2Study.springmvc.itemService.web.interceptor.LogInterceptor;
 import mvc.mvc2Study.springmvc.itemService.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
