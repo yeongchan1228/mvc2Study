657565e459322854a16acebcdfd87ad30e23e220
최영찬 <gks54410@gmail.com>
2022-04-06 16:23:15 +0900

    Feat: Log를 남기기 위한 Filter 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/web/filter/LogFilter.java
@@ -0,0 +1,45 @@
+package mvc.mvc2Study.springmvc.itemService.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+
+            // 다음 필터가 있으면 다음 필터 호출, 다음 필터가 없으면 서블릿 호출
+            // chain.doFilter를 하지 않으면 서블릿 요청 처리가 안되고 그대로 종료된다.
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
