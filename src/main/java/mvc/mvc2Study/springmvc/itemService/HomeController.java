e8ec79ddcd767c0229d6cee07e2637f80bf43d7c
최영찬 <gks54410@gmail.com>
2022-04-06 12:09:52 +0900

    Feat: HttpServlet Session을 사용한 로그인 화면 처리 기능 추가

--- src/main/java/mvc/mvc2Study/springmvc/itemService/HomeController.java
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/HomeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mvc.mvc2Study.springmvc.itemService.domain.member.entity.Member;
 import mvc.mvc2Study.springmvc.itemService.domain.member.repository.MemberRepository;
+import mvc.mvc2Study.springmvc.itemService.web.SessionConst;
 import mvc.mvc2Study.springmvc.itemService.web.session.SessionManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -43,13 +45,26 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model){
 
         Member result = (Member) sessionManager.getSession(request);
         if(result == null) return "home";
 
         model.addAttribute("member", result);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model){
+        HttpSession session = request.getSession(false);
+        if(session == null) return "home";
+
+        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if(member == null) return "home";
+
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
