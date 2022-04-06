cca5b1045ebd2017ae5a1d6d37949e772e0fe531
최영찬 <gks54410@gmail.com>
2022-04-06 12:18:20 +0900

    Feat: 어노테이션을 통한 세션 값 가져오기

--- src/main/java/mvc/mvc2Study/springmvc/itemService/HomeController.java
+++ src/main/java/mvc/mvc2Study/springmvc/itemService/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -55,13 +56,21 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model){
         HttpSession session = request.getSession(false);
         if(session == null) return "home";
 
         Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
+        if(member == null) return "home";
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV4(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member, Model model){
         if(member == null) return "home";
 
         model.addAttribute("member", member);
