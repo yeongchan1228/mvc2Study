52cb9aa74219b753bd10a18e27c6f08fd6bcd914
최영찬 <gks54410@gmail.com>
2022-04-07 15:14:38 +0900

    Feat: RuntimeException 발생 시 BAD_REQUEST 상태 처리 기능 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/exception/api/exception/BadRequestException.java
@@ -0,0 +1,11 @@
+package mvc.mvc2Study.springmvc.exception.api.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+//@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청 오류")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
+public class BadRequestException extends RuntimeException{
+}
