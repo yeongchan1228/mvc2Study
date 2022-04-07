cf3277faa444ce9343b3f2f7a226e5a1ad4a5903
최영찬 <gks54410@gmail.com>
2022-04-07 16:12:54 +0900

    Feat: Spring Converter 작동 확인

---
+++ src/main/java/mvc/mvc2Study/springmvc/converter/controller/HelloController.java
@@ -0,0 +1,28 @@
+package mvc.mvc2Study.springmvc.converter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request, HttpServletResponse response){
+        String data = request.getParameter("data"); // 문자 타입 조회
+        int dataInteger = Integer.parseInt(data); // 문자 -> 숫자
+        log.info("data = {}", dataInteger);
+        return "ok!";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        log.info("data = {}", data.getClass());
+        return "ok!";
+    }
+}
