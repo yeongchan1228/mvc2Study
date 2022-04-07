c92c8c6b3f5bcf6e07de0fe9691a038096a59af4
최영찬 <gks54410@gmail.com>
2022-04-07 16:42:47 +0900

    Test: IpPort<->String Converter 테스트

--- src/test/java/mvc/mvc2Study/springmvc/converter/ConverterTest.java
+++ src/test/java/mvc/mvc2Study/springmvc/converter/ConverterTest.java
@@ -1,8 +1,10 @@
 package mvc.mvc2Study.springmvc.converter;
 
 import mvc.mvc2Study.springmvc.converter.converter.IntegerToStringConverter;
+import mvc.mvc2Study.springmvc.converter.converter.IpPortToStringConverter;
 import mvc.mvc2Study.springmvc.converter.converter.StringToIntegerConverter;
-import org.assertj.core.api.Assertions;
+import mvc.mvc2Study.springmvc.converter.converter.StringToIpPortConverter;
+import mvc.mvc2Study.springmvc.converter.type.IpPort;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -21,5 +23,23 @@ public class ConverterTest {
         IntegerToStringConverter integerToStringConverter = new IntegerToStringConverter();
         String result = integerToStringConverter.convert(10);
         assertThat(result).isInstanceOf(String.class);
+    }
+    
+    @Test
+    public void stringToIpPort() throws Exception {
+        StringToIpPortConverter stringToIpPortConverter = new StringToIpPortConverter();
+        String str = "127.0.0.1:8080";
+        IpPort result = stringToIpPortConverter.convert(str);
+
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    public void IpPortToString() throws Exception {
+        IpPortToStringConverter ipPortToStringConverter = new IpPortToStringConverter();
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        String result = ipPortToStringConverter.convert(ipPort);
+
+        assertThat(result).isEqualTo("127.0.0.1:8080");
     }
 }
