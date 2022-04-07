4167a569b9a8f5c253fb01fbcbfa4df6158754aa
최영찬 <gks54410@gmail.com>
2022-04-07 17:54:19 +0900

    Feat: 사용자 정의 Formatter 등록 추가

--- src/main/java/mvc/mvc2Study/springmvc/converter/config/WebConfig.java
+++ src/main/java/mvc/mvc2Study/springmvc/converter/config/WebConfig.java
@@ -4,17 +4,23 @@ import mvc.mvc2Study.springmvc.converter.converter.IntegerToStringConverter;
 import mvc.mvc2Study.springmvc.converter.converter.IpPortToStringConverter;
 import mvc.mvc2Study.springmvc.converter.converter.StringToIntegerConverter;
 import mvc.mvc2Study.springmvc.converter.converter.StringToIpPortConverter;
+import mvc.mvc2Study.springmvc.converter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
+        // 주석 처리 우선순위 -> Formatter도 String <-> Number 인데 Converter(Integer<->String)를 등록하면 우선 순위에 의해 Formatter가 씹힌다.
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
-        registry.addConverter(new IntegerToStringConverter());
+
+        // Formatter 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
