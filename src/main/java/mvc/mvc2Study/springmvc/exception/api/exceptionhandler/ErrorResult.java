91b7ea45f6b864903b361f0eeefcc52277921c67
최영찬 <gks54410@gmail.com>
2022-04-07 15:31:42 +0900

    Feat: Exception Dto 생성

---
+++ src/main/java/mvc/mvc2Study/springmvc/exception/api/exceptionhandler/ErrorResult.java
@@ -0,0 +1,11 @@
+package mvc.mvc2Study.springmvc.exception.api.exceptionhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
