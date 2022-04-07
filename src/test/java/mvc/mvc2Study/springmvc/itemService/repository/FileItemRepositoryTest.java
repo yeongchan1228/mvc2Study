d0971499f000674d8c05d8672d270a2d2969ffbc
최영찬 <gks54410@gmail.com>
2022-04-07 21:24:06 +0900

    Fix: 클래스명 중복으로 인한 클래스명 변경

--- src/test/java/mvc/mvc2Study/springmvc/itemService/repository/FileItemRepositoryTest.java
+++ src/test/java/mvc/mvc2Study/springmvc/itemService/repository/FileItemRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-class ItemRepositoryTest {
+class FileItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
