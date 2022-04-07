5131646c7249d9c29fcf2e30bb716633e8df8318
최영찬 <gks54410@gmail.com>
2022-04-07 14:32:20 +0900

    Feat: 예외 상황 추가

--- src/main/java/mvc/mvc2Study/springmvc/exception/api/controller/ApiExceptionController.java
+++ src/main/java/mvc/mvc2Study/springmvc/exception/api/controller/ApiExceptionController.java
@@ -3,6 +3,7 @@ package mvc.mvc2Study.springmvc.exception.api.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import mvc.mvc2Study.springmvc.exception.api.exception.UserException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,10 @@ public class ApiExceptionController {
     @GetMapping("/api/members/{id}")
     public MemberDto getMember(@PathVariable String id){
         if(id.equals("ex")) throw new RuntimeException("잘못된 사용자");
+
+        if(id.equals("bad")) throw new IllegalArgumentException("잘못된 입력 값");
+
+        if(id.equals("user-ex")) throw new UserException("사용자 오류");
 
         return new MemberDto(id, "hello " + id);
     }
