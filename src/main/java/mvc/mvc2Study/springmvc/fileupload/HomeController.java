4e7ea37b89f1d417de714ce6243501e032258f88
최영찬 <gks54410@gmail.com>
2022-04-07 18:55:50 +0900

    Feat: FileUpload home Controller 추가

---
+++ src/main/java/mvc/mvc2Study/springmvc/fileupload/HomeController.java
@@ -0,0 +1,13 @@
+package mvc.mvc2Study.springmvc.fileupload;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "fileupload/index";
+    }
+}
